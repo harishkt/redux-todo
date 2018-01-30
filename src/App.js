c89474b64f84ee686331b6173161ef85bec87ebf
hkumar <harisht@udel.edu>
2018-01-30 17:32:18 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+// <img src={logo} className="App-logo" alt="logo" />
+export default class App extends Component {
+	render() {
+		return (
+			<div className="container">
+			</div>
+		);
+	}
+}
